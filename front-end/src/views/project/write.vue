1d94b37bb618224d5a5cea1e60e01dc9b5579eb4
권다은 <daeunkwon@nineonesoft.com>
2021-05-24 10:20:57 +0900

    팀원 추가시 중복 오류 해결 (menu > dialog 변경)

--- front-end/src/views/project/write.vue
+++ front-end/src/views/project/write.vue
@@ -177,93 +177,7 @@
               </v-menu>
             </v-col>
           </v-row>
-          <!-- <v-select
-            outlined
-            v-model="follow_email"
-            :hint="`${follow_email.name}, ${follow_email.email}`"
-            :items="userList"
-            item-text="name"
-            item-value="email"
-            :rules="nameRules"
-            label="팀원"
-            required
-            persistent-hint
-            return-object
-          >
-          </v-select
-          ><br /> -->
 
-          <!-- <v-row>
-            <v-col cols="12" lg="6">
-              <v-menu
-                ref="menu5"
-                v-model="follow_in_d8"
-                :close-on-content-click="false"
-                :return-value.sync="follow_in_date"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="follow_in_date"
-                    label="참여 시작 날짜"
-                    v-bind="attrs"
-                    v-on="on"
-                    outlined
-                  ></v-text-field>
-                </template>
-                <v-date-picker v-model="follow_in_date" no-title scrollable>
-                  <v-spacer></v-spacer>
-                  <v-btn text color="primary" @click="follow_in_d8 = false"
-                    >취소</v-btn
-                  >
-                  <v-btn
-                    text
-                    color="primary"
-                    @click="$refs.menu5.save(follow_in_date)"
-                    >저장</v-btn
-                  >
-                </v-date-picker>
-              </v-menu>
-            </v-col>
-            <v-spacer></v-spacer>
-            <v-col cols="12" lg="6">
-              <v-menu
-                ref="menu6"
-                v-model="follow_out_d8"
-                :close-on-content-click="false"
-                :return-value.sync="lead_out_date"
-                transition="scale-transition"
-                offset-y
-                min-width="290px"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    v-model="follow_out_date"
-                    label="참여 종료 날짜"
-                    v-bind="attrs"
-                    v-on="on"
-                    outlined
-                  ></v-text-field>
-                </template>
-                <v-date-picker v-model="follow_out_date" no-title scrollable>
-                  <v-spacer></v-spacer>
-                  <v-btn text color="primary" @click="follow_out_d8 = false"
-                    >취소</v-btn
-                  >
-                  <v-btn
-                    text
-                    color="primary"
-                    @click="$refs.menu6.save(follow_out_date)"
-                    >저장</v-btn
-                  >
-                </v-date-picker>
-              </v-menu>
-            </v-col>
-          </v-row> -->
-
-          <!--test-->
           <v-row v-for="(input, k) in inputs" :key="k">
             <v-col cols="12">
               <v-select
@@ -282,79 +196,78 @@
               </v-select
             ></v-col>
             <v-col cols="12">
-              <v-menu
-                ref="menu7"
+              <v-dialog
+                ref="dialog"
                 v-model="modal"
-                :close-on-content-click="false"
                 :return-value.sync="input.in_date"
+                persistent
+                width="290px"
               >
                 <template v-slot:activator="{ on, attrs }">
                   <v-text-field
                     v-model="input.in_date"
                     :id="'in_date' + k"
                     label="참여 시작 날짜"
+                    readonly
                     v-bind="attrs"
                     v-on="on"
                     outlined
                   ></v-text-field>
                 </template>
                 <v-date-picker
+                  v-if="modal"
                   v-model="input.in_date"
                   :id="'in_date' + k"
-                  no-title
-                  scrollable
-                  width="467"
+                  full-width
                 >
                   <v-spacer></v-spacer>
                   <v-btn text color="primary" @click="modal = false"
-                    >취소</v-btn
+                    >Cancel</v-btn
                   >
                   <v-btn
                     text
                     color="primary"
-                    @click="$refs.menu7[k].save(input.in_date)"
-                    >저장</v-btn
+                    @click="$refs.dialog[k].save(input.in_date)"
+                    >OK</v-btn
                   >
                 </v-date-picker>
-              </v-menu>
-              <v-spacer></v-spacer>
-              <v-menu
-                ref="menu8"
+              </v-dialog>
+
+              <v-dialog
+                ref="dialog2"
                 v-model="modal2"
-                :close-on-content-click="false"
                 :return-value.sync="input.out_date"
-                transition="scale-transition"
-                offset-y
+                persistent
+                width="290px"
               >
                 <template v-slot:activator="{ on, attrs }">
                   <v-text-field
                     v-model="input.out_date"
                     :id="'out_date' + k"
                     label="참여 종료 날짜"
+                    readonly
                     v-bind="attrs"
                     v-on="on"
                     outlined
                   ></v-text-field>
                 </template>
                 <v-date-picker
+                  v-if="modal2"
                   v-model="input.out_date"
-                  :id="'out_date' + k"
-                  no-title
-                  scrollable
-                  width="467"
+                  full-width
                 >
                   <v-spacer></v-spacer>
                   <v-btn text color="primary" @click="modal2 = false"
-                    >취소</v-btn
+                    >Cancel</v-btn
                   >
                   <v-btn
                     text
                     color="primary"
-                    @click="$refs.menu8[k].save(input.out_date)"
-                    >저장</v-btn
+                    @click="$refs.dialog2[k].save(input.out_date)"
+                    >OK</v-btn
                   >
                 </v-date-picker>
-              </v-menu>
+              </v-dialog>
 
               <v-btn
                 class="sm-2 mr-4"
@@ -381,22 +294,6 @@
             </v-col>
             <br />
           </v-row>
-
-          <!--test-->
-
-          <!-- <v-btn
-            class="sm-2 mr-4"
-            fab
-            dark
-            small
-            color="indigo"
-            @click="remove"
-          >
-            <v-icon dark>mdi-minus</v-icon>
-          </v-btn>
-          <v-btn class="sm-2" fab dark small color="indigo" @click="add">
-            <v-icon dark>mdi-plus</v-icon>
-          </v-btn> -->
 
           <br /><br /><br />
 
@@ -440,6 +337,8 @@ export default {
   },
   data() {
     return {
+      dialog: false,
+      dialog2: false,
       start_d8: false,
       end_d8: false,
       prj_title: "",
@@ -463,8 +362,6 @@ export default {
       menu2: false,
       menu3: false,
       menu4: false,
-      menu5: false,
-      menu6: false,
       remark: "",
       content: "",
 
